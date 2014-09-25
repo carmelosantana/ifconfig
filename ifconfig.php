f860c630ec158a5e9daf0ef14343e8403c2af102
Carmelo <me@carmelosantana.com>
2014-09-25 16:18:31 -0400

    Cleaned up output. Removed ADDR when IP is the same. Added Github update headers.

--- ifconfig.php
+++ ifconfig.php
@@ -3,13 +3,24 @@
 Plugin Name: ifconfig
 Plugin URI: http://www.carmelosantana.com
 Description: Display IP and host information.
-Version: 0.1
+Version: 0.2
 Author: @carmelosantana
 Author URI: http://www.carmelosantana.com
+GitHub Plugin URI: carmelosantana/ifconfig
+GitHub Branch: master
 */
 
 function ifc_footer($out=null){
-	return $out . '<p class="alignleft">IP <code>' . $_SERVER['SERVER_ADDR'] . '</code> | ' . 'ADDR <code>' . gethostbyname($_SERVER['SERVER_NAME']) . '</code> | ' . 'Host <code>' . gethostname() . '</code></p>';
+	// start IP
+	$out.= ' IP <code class="alternate">' . $_SERVER['SERVER_ADDR'] . '</code>';
+
+	// if IP + ADDR are same, only do IP
+	$add = gethostbyname($_SERVER['SERVER_NAME']);
+	if ( $_SERVER['SERVER_ADDR'] !== $add )
+		$out.= ' ADDR <code class="alternate">' . $add . '</code>';
+
+	// back
+	return $out . ' Host <code class="alternate">' . gethostname() . '</code>';
 }
 
 add_filter('admin_footer_text', 'ifc_footer');
