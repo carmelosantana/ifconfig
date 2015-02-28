206856767d572c3937c38fcfeb3816824ff24ed3
Carmelo <me@carmelosantana.com>
2015-02-27 20:33:17 -0500

    Cleaned up output. Removed github links. Updated user plugin URL.

--- ifconfig.php
+++ ifconfig.php
@@ -1,26 +1,26 @@
 <?php 
 /*
 Plugin Name: ifconfig
-Plugin URI: http://www.carmelosantana.com
+Plugin URI: http://www.carmelosantana.com/plugins/
 Description: Display IP and host information.
-Version: 0.2
+Version: 0.3
 Author: @carmelosantana
 Author URI: http://www.carmelosantana.com
-GitHub Plugin URI: carmelosantana/ifconfig
-GitHub Branch: master
 */
 
-function ifc_footer($out=null){
-	// start IP
-	$out.= ' IP <code class="alternate">' . $_SERVER['SERVER_ADDR'] . '</code>';
+function ifc_footer($def=null){
+	$out = '<p class="alignleft">' . $def;
+	foreach (ifc_define() as $key => $value)
+		$out.= ' ' . $key . ' <code class="alternate">' . $value . '</code>';
 
-	// if IP + ADDR are same, only do IP
-	$add = gethostbyname($_SERVER['SERVER_NAME']);
-	if ( $_SERVER['SERVER_ADDR'] !== $add )
-		$out.= ' ADDR <code class="alternate">' . $add . '</code>';
+	return $out . '</p>';
+}
 
-	// back
-	return $out . ' Host <code class="alternate">' . gethostname() . '</code>';
+function ifc_define(){
+	return array(
+		'IP' => $_SERVER['SERVER_ADDR'],
+		'Host' => gethostbyname($_SERVER['SERVER_NAME']),
+	);
 }
 
 add_filter('admin_footer_text', 'ifc_footer');
